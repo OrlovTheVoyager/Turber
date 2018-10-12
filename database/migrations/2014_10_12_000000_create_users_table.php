ee9f2670a7137712eb3990dcb2d648df66ee0e94
Igor Orlov <1.orlovigor@gmail.com>
2018-10-12 19:39:55 +0200

    migrations setup: users, posts
    Signed-off-by: Igor Orlov <1.orlovigor@gmail.com>

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,10 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('type');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('licence')->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
